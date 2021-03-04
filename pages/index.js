ae166785bf9039585c56f9dab4b2e8c9d4b158cb
lucas-goldner <lucas.goldner@googlemail.com>
2021-03-04 04:34:59 +0100

    feat: Styled about section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout from "../container/Layout";
 import Hero from "../container/Hero";
+import About from "../container/About";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
       <Layout>
         <Hero id="home" />
+        <About id="about" />
       </Layout>
     </div>
   );
