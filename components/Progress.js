d02062ee1b852b1314962a4dd4e33a6ce767ae42
lucas-goldner <lucas.goldner@googlemail.com>
2021-03-06 05:39:23 +0100

    feat: Progress shown

--- components/Progress.js
+++ components/Progress.js
@@ -1,35 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import handleViewport from "react-in-viewport";
 import CountUp from "react-countup";
 
-function Progress({ name, value, delay }) {
-  const [thisValue, setThisValue] = useState(value);
-  const [inViewport, setInViewport] = useState(false);
-  const [animationComplete, setAnimationComplete] = useState(false);
+class Progress_Animation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      inViewport: false,
+      animation_complete: false,
+    };
+  }
 
-  useEffect(() => {
-    if (inViewport !== inViewport && !animationComplete) {
-      setInViewport(inViewport);
-      setAnimationComplete(animationComplete);
-      showProgress();
+  componentDidUpdate() {
+    if (
+      this.state.inViewport !== this.props.inViewport &&
+      !this.state.animation_complete
+    ) {
+      this.setState({ inViewport: this.props.inViewport });
+      this.setState({ animation_complete: true });
+      this.showProgress();
     }
-  }, []);
+  }
 
-  const showProgress = () => {
+  showProgress() {
     setTimeout(() => {
-      setThisValue(value);
-    }, delay);
-  };
+      this.setState({ value: this.props.value });
+    }, this.props.delay);
+  }
 
-  return (
-    <div className="progress-container">
-      <span className="name">{name}</span>
-      <span className="value">
-        <CountUp start={0} end={inViewport === true ? thisValue : 0} />%
-      </span>
-      <div className="progress" style={{ width: `${thisValue}%` }}></div>
-    </div>
-  );
+  render() {
+    const { name } = this.props;
+    return (
+      <div className="progress-container">
+        <span className="name">{name}</span>
+        <span className="value">
+          <CountUp
+            start={0}
+            end={this.state.inViewport === true ? this.state.value : 0}
+          />
+          %
+        </span>
+        <div
+          className="progress"
+          style={{ width: `${this.state.value}%` }}
+        ></div>
+      </div>
+    );
+  }
 }
+const Progress = handleViewport(Progress_Animation);
 
-export default Progress = handleViewport(Progress);
+export default Progress;
