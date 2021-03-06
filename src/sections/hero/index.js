9440e81bd450b70e909d60200324f540ed67e2c4
lucas-goldner <lucas.goldner@googlemail.com>
2021-03-06 23:49:39 +0100

    feat: Change Hero Info

--- src/sections/hero/index.js
+++ src/sections/hero/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import './styles.scss'
-import { StaticQuery, graphql } from 'gatsby'
-import { Row, Col } from 'react-bootstrap'
-import Glitch from 'components/glitch'
-import Typewriter from 'typewriter-effect'
-import ThemeContext from '../../context'
-class Hero extends React.Component {
+import Glitch from "components/glitch";
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import Typewriter from "typewriter-effect";
+import ThemeContext from "../../context";
+import heroData from "../../../data/hero.json";
+import "./styles.scss";
 
-  static contextType = ThemeContext
+class Hero extends React.Component {
+  static contextType = ThemeContext;
 
   render() {
     return (
@@ -20,88 +20,59 @@ class Hero extends React.Component {
           <Col md={6} className="content">
             <div className="content-text">
               <div className="line-text">
-                <h4>Hello, I'm</h4>
+                <h4>俺は</h4>
               </div>
-              <Glitch text="John Neo" />
+              <Glitch text="Lucas Goldner" />
               <Typewriter
                 options={{
                   strings: [
-                    'UI/UX Designer',
-                    'PHP Developer',
-                    'Mobile App Expert',
+                    "IOS Developer",
+                    "Front End Engineer",
+                    "Android Developer",
                   ],
                   autoStart: true,
                   loop: true,
                 }}
               />
               <button className="hover-button">
-                  <span>Download CV</span>
+                <span>Download CV</span>
               </button>
             </div>
             {this.icons()}
           </Col>
           <Col md={6} className="img">
             <img
-              src={this.props.mainImg.childImageSharp.fluid.src}
+              src={heroData.data.Img.childImageSharp.fluid.src}
               alt="person"
             />
           </Col>
         </Row>
       </section>
-    )
+    );
   }
 
   icons() {
-    return this.props.icons.edges.map((value, index) => {
+    return heroData.data.icons.edges.map((value, index) => {
       return (
         <img
           src={value.node.childImageSharp.fluid.src}
           className={`animated fadeIn move-${
-            Math.floor(Math.random() * 10) % 2 === 0 ? 'up' : 'down'
+            Math.floor(Math.random() * 10) % 2 === 0 ? "up" : "down"
           } float-image`}
           style={{
             left: `${index * 10}%`,
-            bottom: `${Math.random() *
-              (+(index % 2 === 0 ? 80 : 20) - +(index % 2 === 0 ? 70 : 10)) +
-              +(index % 2 === 0 ? 70 : 10)}%`,
+            bottom: `${
+              Math.random() *
+                (+(index % 2 === 0 ? 80 : 20) - +(index % 2 === 0 ? 70 : 10)) +
+              +(index % 2 === 0 ? 70 : 10)
+            }%`,
           }}
           alt="shape"
           key={index}
         />
-      )
-    })
+      );
+    });
   }
 }
 
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query {
-        icons: allFile(
-          filter: {
-            extension: { regex: "/(png)/" }
-            relativeDirectory: { eq: "icons" }
-          }
-        ) {
-          edges {
-            node {
-              childImageSharp {
-                fluid(maxWidth: 100) {
-                  src
-                }
-              }
-            }
-          }
-        }
-        Img: file(relativePath: { eq: "person.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 2000) {
-              src
-            }
-          }
-        }
-      }
-    `}
-    render={({ icons, Img }) => <Hero icons={icons} mainImg={Img} {...props} />}
-  />
-)
+export default Hero;
