551d46a117495b5dca2c3c9c002f9046cd2fbfb4
lucas-goldner <lucas.goldner@googlemail.com>
2021-03-07 01:24:27 +0100

    feat: Adjust about section

--- src/components/progress/index.js
+++ src/components/progress/index.js
@@ -1,44 +1,55 @@
-import React from 'react'
-import handleViewport from 'react-in-viewport'
-import CountUp from 'react-countup'
-import './styles.scss'
+import React from "react";
+import handleViewport from "react-in-viewport";
+import CountUp from "react-countup";
+import "./styles.scss";
 
 class Progress_Animation extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: 0,
-            inViewport: false,
-            animation_complete: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      inViewport: false,
+      animation_complete: false,
+    };
+  }
 
-    componentDidUpdate() {
-        if (this.state.inViewport !== this.props.inViewport && !this.state.animation_complete) {
-            this.setState({inViewport: this.props.inViewport})
-            this.setState({animation_complete: true})
-            this.showProgress()
-        }
+  componentDidUpdate() {
+    if (
+      this.state.inViewport !== this.props.inViewport &&
+      !this.state.animation_complete
+    ) {
+      this.setState({ inViewport: this.props.inViewport });
+      this.setState({ animation_complete: true });
+      this.showProgress();
     }
+  }
 
-    showProgress() {
-        setTimeout(() => { 
-            this.setState({value : this.props.value})
-        }, this.props.delay);
-    }
+  showProgress() {
+    setTimeout(() => {
+      this.setState({ value: this.props.value });
+    }, this.props.delay);
+  }
 
-    
-    render() {
-        const { name } = this.props
-        return(
-            <div className="progress-container">
-                <span className="name">{name}</span>
-                <span className="value"><CountUp start={0} end={this.state.inViewport === true ? this.state.value : 0} />%</span>
-                <div className="progress" style={{width: `${this.state.value}%`}}></div>
-            </div>
-        )
-    }
+  render() {
+    const { name } = this.props;
+    return (
+      <div className="progress-container">
+        <span className="name">{name}</span>
+        <span className="value">
+          <CountUp
+            start={0}
+            end={this.state.inViewport === true ? this.state.value : 0}
+          />{" "}
+          {this.state.value == 1 ? " Year" : " Years"}
+        </span>
+        <div
+          className="progress"
+          style={{ width: `${this.state.value * 20}%` }}
+        ></div>
+      </div>
+    );
+  }
 }
 const Progress = handleViewport(Progress_Animation);
 
-export default Progress
+export default Progress;
--- src/sections/about/index.js
+++ src/sections/about/index.js
@@ -1,81 +1,117 @@
-import React from 'react'
-import Particles from 'react-particles-js';
-import Progress from 'components/progress'
-import { Row, Col } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
-import ThemeContext from '../../context'
-import './styles.scss'
+import React from "react";
+import Particles from "react-particles-js";
+import Progress from "components/progress";
+import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import ThemeContext from "../../context";
+import "./styles.scss";
 
 class Hero extends React.Component {
+  static contextType = ThemeContext;
 
-    static contextType = ThemeContext
+  render() {
+    return (
+      <section
+        id={`${this.props.id}`}
+        className="about"
+        style={{ height: this.context.height }}
+      >
+        {this.particles()}
+        <Row>
+          <Col md={6} className="content">
+            <div className="content-text">
+              <div className="line-text">
+                <h4>About Me</h4>
+              </div>
+              <h3>I'm an App and Web Developer</h3>
+              <div className="separator" />
+              <p>
+                Creating something from the ground up, completly by myself, has
+                always been my passion. I started to get into programming by
+                making my own small game in Unity. After that I started learning
+                Java and got into Android App Development, mainly because I am
+                Co-Founder of <a href="https://flowus.app">FlowUS</a>. I also
+                learnt all about web development HTML, CSS, JS, React and other
+                frameworks, in order to code our landing page for the app. By
+                working as a Front End Engineer, I made enough money to buy me a
+                Mac and IPhone, so I can also assist my team in the IOS
+                Development department. Along the way I learnt some languages,
+                like Python and Go for microservices.
+              </p>
+              <div className="social social_icons">
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  className="social_icon"
+                  onClick={() => window.open("https://www.github.com")}
+                />
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  className="social_icon"
+                  onClick={() => window.open("https://www.twitter.com")}
+                />
+                <FontAwesomeIcon
+                  icon={faYoutube}
+                  className="social_icon"
+                  onClick={() => window.open("https://www.youtube.com")}
+                />
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  className="social_icon"
+                  onClick={() => window.open("https://www.linkedin.com")}
+                />
+              </div>
+            </div>
+          </Col>
+          <Col md={6} className="skills">
+            <div className="line-text">
+              <h4>My Skills</h4>
+            </div>
+            <div className="skills-container">
+              <Progress name="JavaScript" value={2} delay={1100} />
+              <Progress name="Java" value={4} delay={1100} />
+              <Progress name="Swift" value={1} delay={1100} />
+              <Progress name="Go" value={1} delay={1100} />
+              <Progress name="Python" value={1} delay={1100} />
+              <Progress name="C#" value={4} delay={1100} />
+            </div>
+          </Col>
+        </Row>
+      </section>
+    );
+  }
 
-    render() {
-        return (
-            <section id={`${this.props.id}`} className="about" style={{height: this.context.height}}>
-                {this.particles()}
-                <Row>
-                    <Col md={6} className="content">
-                        <div className="content-text">
-                            <div className="line-text">
-                                <h4>About Me</h4>
-                            </div>
-                            <h3>I'm a Full Stack web developer working from home</h3>
-                            <div className="separator" />
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit nisi vitae feugiat vestibulum. Aliquam porta nulla vel odio scelerisque, pretium volutpat dui euismod. Integer porttitor dolor placerat malesuada dictum. Fusce enim dolor, dignissim quis ornare at, elementum nec turpis. Donec ac interdum libero, sed condimentum lectus. Nunc nec iaculis tortor. Donec interdum sollicitudin eros in pharetra. Donec ultricies laoreet dictum. Maecenas vestibulum sodales justo, id hendrerit orci aliquet gravida. Nulla facilisi.</p>
-                            <div className="social social_icons">
-                                <FontAwesomeIcon icon={faGithub} className="social_icon" onClick={() => window.open('https://www.github.com')}/>
-                                <FontAwesomeIcon icon={faTwitter} className="social_icon" onClick={() => window.open('https://www.twitter.com')} />
-                                <FontAwesomeIcon icon={faYoutube} className="social_icon" onClick={() => window.open('https://www.youtube.com')} />
-                                <FontAwesomeIcon icon={faLinkedin} className="social_icon" onClick={() => window.open('https://www.linkedin.com')} />
-                            </div>
-                        </div>
-                    </Col>
-                    <Col md={6} className="skills">
-                            <div className="line-text">
-                                <h4>My Skills</h4>
-                            </div>
-                            <div className="skills-container">
-                                <Progress name="Web Design" value={90} delay={1100} />
-                                <Progress name="Angular" value={50} delay={1100} />
-                                <Progress name="React" value={80} delay={1100} />
-                                <Progress name="Vue" value={40} delay={1100} />
-                                <Progress name="MongoDB" value={100} delay={1100} />
-                                <Progress name="CSS" value={50} delay={1100} />
-                            </div>
-                    </Col>
-                </Row>
-            </section>
-        )
-    }
-
-    particles() {
-        return (
-            <Particles
-                className="particles"
-                params={{
-                    "particles": {
-                        "number": {
-                            "value": 50,
-                            "density": {
-                                "enable": false,
-                                "value_area": 5000
-                            }
-                        },
-                        "line_linked": {
-                            "enable": true,
-                            "opacity": .5
-                        },
-                        "size": {
-                            "value": 1
-                        },
-                    },
-                    "retina_detect": true
-            }}/>
-        )
-    }
-
+  particles() {
+    return (
+      <Particles
+        className="particles"
+        params={{
+          particles: {
+            number: {
+              value: 50,
+              density: {
+                enable: false,
+                value_area: 5000,
+              },
+            },
+            line_linked: {
+              enable: true,
+              opacity: 0.5,
+            },
+            size: {
+              value: 1,
+            },
+          },
+          retina_detect: true,
+        }}
+      />
+    );
+  }
 }
 
-export default Hero
+export default Hero;
