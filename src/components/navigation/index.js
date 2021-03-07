9ff032ce924c3bb48114bf759954e500144c46b7
lucas-goldner <lucas.goldner@googlemail.com>
2021-03-07 05:37:24 +0100

    feat: Add new Lucas Logo

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,80 +1,85 @@
-import React from 'react'
-import './styles.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons'
-var scrollToElement = require('scroll-to-element')
+import React from "react";
+import "./styles.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
+var scrollToElement = require("scroll-to-element");
 
 class Navigation extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            show: false
-        }
-        this.sections = [
-            {
-                name: "Home"
-            },
-            {
-                name: "About"
-            },
-            {
-                name: "Services"
-            },
-            {
-                name: "Portfolio"
-            },
-            {
-                name: "Testimonials"
-            },
-            {
-                name: "Contact"
-            }
-        ]
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+    this.sections = [
+      {
+        name: "Home",
+      },
+      {
+        name: "About",
+      },
+      {
+        name: "Services",
+      },
+      {
+        name: "Portfolio",
+      },
+      {
+        name: "Contact",
+      },
+    ];
+  }
 
-    navScroll(id, v) {
-        
-        this.setState({show: false})
-        const el = document.getElementById(id)
-        scrollToElement(el, {
-            offset: 0,
-            ease: 'in-out-expo',
-            duration: 2000
-          }).on('end', () => {
-            this.props.change(v)
-        });
-    }
+  navScroll(id, v) {
+    this.setState({ show: false });
+    const el = document.getElementById(id);
+    scrollToElement(el, {
+      offset: 0,
+      ease: "in-out-expo",
+      duration: 2000,
+    }).on("end", () => {
+      this.props.change(v);
+    });
+  }
 
+  render() {
+    return (
+      <div>
+        <div className="opener">
+          <FontAwesomeIcon
+            icon={faBars}
+            className="closeNav"
+            onClick={() => this.setState({ show: true })}
+          />
+        </div>
+        <div className={`navigation ${this.state.show ? "active" : ""}`}>
+          <FontAwesomeIcon
+            icon={faTimes}
+            className="closeNav"
+            onClick={() => this.setState({ show: false })}
+          />
+          <div className="logo">
+            <img src="img/logo.png" alt="logo" />
+          </div>
+          <div className="links">
+            <ul>{this.items()}</ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-    render() {
-        return(
-            <div>
-                <div className="opener">
-                    <FontAwesomeIcon icon={faBars} className="closeNav" onClick={() => this.setState({show: true})} />
-                </div>
-                <div className={`navigation ${this.state.show ? "active" : ""}`}>
-                    <FontAwesomeIcon icon={faTimes} className="closeNav" onClick={() => this.setState({show: false})} />
-                    <div className="logo">
-                        <img src="img/logo.png" alt="logo"/>
-                    </div>
-                    <div className="links">
-                        <ul>
-                            {this.items()}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
-    items() {
-        return this.sections.map((value, index) => {
-            return (
-                <li key={index}>
-                    <button onClick={() => this.navScroll(value.name.toLowerCase(), index)}>{value.name}</button>
-                </li>
-            )
-        })
-    }
+  items() {
+    return this.sections.map((value, index) => {
+      return (
+        <li key={index}>
+          <button
+            onClick={() => this.navScroll(value.name.toLowerCase(), index)}
+          >
+            {value.name}
+          </button>
+        </li>
+      );
+    });
+  }
 }
-export default Navigation
+export default Navigation;
